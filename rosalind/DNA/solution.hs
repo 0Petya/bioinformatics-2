8322560c8c394014da78937b64ee7d0e19ef64b3
Peter Tran <Peter1992t@gmail.com>
2020-01-14 14:50:59 -0600

    Initialize repo; solve first rosalind problem

---
+++ rosalind/DNA/solution.hs
@@ -0,0 +1,18 @@
+import Data.List (intercalate)
+import Data.Map.Strict (Map, fromList, (!), adjust)
+
+count :: Char -> Map Char Int -> Map Char Int
+count nucleotide counts =
+    adjust ((+) 1) nucleotide counts
+
+solve :: String -> String
+solve sequence =
+    intercalate " " $ map (show . (!) counts) ['A', 'C', 'G', 'T']
+    where counts = foldr count (fromList [(x, 0) | x <- ['A', 'C', 'G', 'T']]) sequence
+
+main :: IO()
+main = do
+    putStrLn "Insert file name:"
+    fileName <- getLine
+    content <- readFile fileName
+    putStrLn . solve $ content
